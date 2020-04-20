e490df243676626f3c56abe63544c7d87e78c888
Maren Parnas Gulnes <maren.gulnes@bouvet.no>
2020-04-20 18:08:44 +0200

    Updates

---
+++ precache-manifest.00c5d878992e369e9e34baae89fdb6be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a916472d870e4faf047f0cf8a1028c4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d477210cbe26357e413",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8d69e3ca5b32c8924c3b",
+    "url": "/static/js/2.956a0898.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.956a0898.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d477210cbe26357e413",
+    "url": "/static/js/main.0192462e.chunk.js"
+  },
+  {
+    "revision": "52b2b9893b8fea52d6cf",
+    "url": "/static/js/runtime-main.95188e81.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/kvikkfikspage/precache-manifest.29b4ca565e5b5bc77d55c544526c36c5.js"
+  "/precache-manifest.00c5d878992e369e9e34baae89fdb6be.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/kvikkfikspage/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
